0d15d1ac05cdd25b32c6d78f08e4add4890bdaaa
Your Name <you@example.com>
2018-06-13 18:25:44 -0700

    Adding winning combo list

--- ttt.py
+++ ttt.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-
+winning_combo = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 pygame.init()
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption("TTT")
